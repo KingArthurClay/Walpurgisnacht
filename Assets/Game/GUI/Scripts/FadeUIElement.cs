15e0c76218610a523e9f58afcc15d1d4baac017d
KingNullpointer <KingNullpointer@users.noreply.github.com>
2019-11-20 17:04:20 -0500

    Safekeeping for UI Prefab Changes

---
+++ Assets/Game/GUI/Scripts/FadeUIElement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeUIElement : MonoBehaviour
+{
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.layer == 0)
+        {
+            //Enter Fade-Out Animation
+        }
+    }
+
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.layer == 0)
+        {
+            //Exit fade-out animation
+        }
+    }
+
+}
